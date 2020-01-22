64d68dfb4707f58f7bb71c79aece2bd021ed949e
Stefan Savev <stefan.savev@originate.com>
2020-01-22 21:11:39 +0100

    fix bugs

---
+++ mnt/README.txt
@@ -0,0 +1,2 @@
+A directory called data will be created here from the docker container.
+The directory mnt/data will be mounted in docker.
--- run-notebook.sh
+++ run-notebook.sh
@@ -6,12 +6,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 pushd $DIR
 
+mkdir -p mnt/data
+
 docker run \
--v `pwd`/data:/data \
+ -u $(id -u):$(id -g) \
+-v `pwd`/mnt/data:/data \
 -v `pwd`/notebooks:/notebooks/dbg-pds-demo \
 -m 8g \
 -it -p 8888:8888 \
-dbg-pds-tensorflow-demo 
+dbg-pds-tensorflow-demo
 
 popd
 
